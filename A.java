f34c2f48e43ccfcaf9bfb3f8f4493f69eb64d88b
abc <yaswanthbs@users.noreply.github.com>
2021-03-11 10:09:49 +0530

    Update A.java

--- A.java
+++ A.java
@@ -12,6 +12,6 @@ public class A {
 	public static void main(String[] args) {
 		B b = new B();
 		A a = (A) b;
-		System.out.println(a.getName() + "," + b.getName());
+		System.out.println(a.getName() + "," + b.getName()); //B,B
 	}
 }

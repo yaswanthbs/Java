90dcd83482f680b7b18b163fc545be4411470b3d
abc <yaswanthbs@users.noreply.github.com>
2021-03-11 10:39:23 +0530

    Create WithoutNumbers.java

---
+++ WithoutNumbers.java
@@ -0,0 +1,15 @@
+public class WithoutNumbers {
+
+	// Print 1 to 100 without numbers
+
+	public static void main(String a[]) {
+		
+		int number = 'A'/'A';
+		String forLength = "tententent";
+
+		for (int i = number; i <= forLength.length() * forLength.length(); i++) {
+			System.out.println(i);
+		}
+
+	}
+}
